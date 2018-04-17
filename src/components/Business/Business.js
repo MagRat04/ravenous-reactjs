8500c803b8b237b3a22d4984a682e03f500b2bba
Chase Gatyas <jcgatyas@techsoft.com>
2018-04-17 14:24:26 -0500

    completed Ravenous III, handleSortByChange was not written properly in the solution code

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -5,20 +5,20 @@ class Business extends React.Component {
     render() {
         return (
             <div className="Business">
-                <div class="image-container">
-                    <img src='{this.business.imageSrc}' alt='' />
+                <div className="image-container">
+                    <img src={this.props.business.imageSrc} alt='' />
                 </div>
                 <h2>{this.props.business.name}</h2>
                 <div className="Business-information">
                     <div className="Business-address">
                         <p>{this.props.business.address}</p>
                         <p>{this.props.business.city}</p>
-                        <p>{this.props.business.state} {this.props.business.zipCode}</p>
+                        <p>{`${this.props.business.state} ${this.props.business.zipCode}`}</p>
                     </div>
                     <div className="Business-reviews">
                         <h3>{this.props.business.category}</h3>
-                        <h3 className="rating">{this.props.business.rating}</h3>
-                        <p>{this.props.business.reviewCount} reviews</p>
+                        <h3 className="rating">{`${this.props.business.rating} stars`}</h3>
+                        <p>{`${this.props.business.reviewCount} reviews`}</p>
                     </div>
                 </div>
             </div>    
